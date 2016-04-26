8bd05af272ad1e0034c896a598a735d23dd560ed
Wilon <cvl556@126.com>
2016-04-26 11:30:42 +0800

    Initial Commit

---
+++ TestKeyBord/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestKeyBord
+//
+//  Created by 陈威 on 16/4/26.
+//  Copyright © 2016年 Wilon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestKeyBord/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestKeyBord
+//
+//  Created by 陈威 on 16/4/26.
+//  Copyright © 2016年 Wilon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
